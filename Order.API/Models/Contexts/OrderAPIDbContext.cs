9cd273307ba4d472576c97a4606c1f8b776ce574
Yasin Çoban <cobanyasin1998@gmail.com>
2024-10-09 22:39:08 +0300

    Add project files.

---
+++ Order.API/Models/Contexts/OrderAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Order.API.Models.Contexts
+{
+    public class OrderAPIDbContext : DbContext
+    {
+        public OrderAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,59 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared;
+using Shared.Events;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers;
+
+public class OrderCreatedEventConsumer(MongoDBService mongoDBService, ISendEndpointProvider sendEndpointProvider,IPublishEndpoint publishEndpoint) : IConsumer<OrderCreatedEvent>
+{
+
+    public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+    {
+        List<bool> stockResult = new();
+        IMongoCollection<Stock.API.Models.Stock> collection = mongoDBService.GetCollection<Stock.API.Models.Stock>();
+
+        foreach (var orderItem in context.Message.OrderItems)
+        {
+            stockResult.Add(await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId.ToString() && s.Count >= orderItem.Count)).AnyAsync());
+        }
+        if (stockResult.TrueForAll(s => s.Equals(true)))
+        {
+            //Stock Güncellemesi
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                Models.Stock stock = await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId.ToString())).FirstOrDefaultAsync();
+
+                stock.Count -= orderItem.Count;
+
+                await collection.ReplaceOneAsync(s => s.ProductId == orderItem.ProductId.ToString(), stock);
+
+            }
+            //Payment i uyaracak event'in fırlatılması
+            var sendEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.Payment_StockReservedEventQueue}"));
+            await sendEndpoint.Send<StockReservedEvent>(new()
+            {
+                OrderId = context.Message.OrderId,
+                BuyerId = context.Message.BuyerId,
+                TotalPrice = context.Message.TotalPrice,
+                OrderItems = context.Message.OrderItems
+            });
+        }
+        else
+        {
+            //Stok yetersiz
+            //Order i uyaracak event in fırlatılması
+            StockNotReservedEvent stockNotReservedEvent = new()
+            {
+                OrderId = context.Message.OrderId,
+                BuyerId = context.Message.BuyerId,
+                Message = "Stock is not enough"
+            };
+
+            await publishEndpoint.Publish<StockNotReservedEvent>(stockNotReservedEvent);
+
+        }
+
+    }
+}
